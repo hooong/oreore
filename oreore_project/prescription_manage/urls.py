31b2cd19a2509282571d2daa44fc79be37df711e
hooong <tjrwns0529@gmail.com>
2019-11-01 18:16:25 +0900

    add medicine json data

--- oreore_project/prescription_manage/urls.py
+++ oreore_project/prescription_manage/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('all/', views.all_prescription, name="all_prescription"),
     path('add/', views.add_prescription, name='add_prescription'),
     path('detail/<int:pre_id>', views.detail_prescription, name='detail_prescription'),
-    path('mod/', views.mod_medicine, name="mod_medicine")
+    path('mod/<str:code>/<int:pre_id>/', views.mod_medicine_view, name="mod_medicine_view"),
+    path('mod_save/', views.mod_medicine, name="mod_medicine")
 ]
--- oreore_project/prescription_manage/views.py
+++ oreore_project/prescription_manage/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import *
 from disease_manage.models import *
 from medicine_manage.models import *
+import json, os
+from oreore_project.settings import BASE_DIR
 
 def all_prescription(request):
     # 처방전 (로그인 한 사람만 들어올수있는페이지)
@@ -36,8 +38,33 @@ def detail_prescription(request, pre_id):
 
     return render(request, 'detail_prescription.html', context)
 
+def mod_medicine_view(request, code, pre_id):
+    context = {'code':code, 'pre_id':pre_id}
+
+    return render(request, "mod_medicine.html", context)
+
 def mod_medicine(request):
-    if request.method == 'POST':
-        return 0
-    else:
-        return render(request, "mod_medicine.html")
+    pre_id = request.POST['pre_id']
+    medi_name = request.POST['medi_name']
+    code = request.POST['insurcode']
+
+    # json data 수정 및 추가
+    medicine = dict()
+    medicine["name"] = medi_name
+
+    filename = os.path.join(BASE_DIR,'data/medicine_data.json')
+    with open(filename, 'r') as f:
+        data = json.load(f)
+        data[code] = medicine
+
+    os.remove(filename)
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+    # db내용 수정
+    pre = Prescription.objects.get(id=pre_id)
+    medicine = Medicine.objects.get(linkPrescription=pre, insuranceCode=code)
+    medicine.mediName = medi_name
+    medicine.save()
+        
+    return redirect('/prescription/detail/'+str(pre_id))
